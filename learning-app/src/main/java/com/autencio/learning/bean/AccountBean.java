71c72569008b962b5bef8b581d1743110201ea8b
Gwen Harold Autencio <gwen.autencio@gmail.com>
2015-12-18 01:26:00 +1100

    add bean

--- learning-app/src/main/java/com/autencio/learning/bean/AccountBean.java
+++ learning-app/src/main/java/com/autencio/learning/bean/AccountBean.java
@@ -2,4 +2,13 @@ package com.autencio.learning.bean;
 
 public class AccountBean {
 
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 }
