9a1b6b2dda3a476ce76fe2ac0e8cf77d6d1e8599
Gwen Harold Autencio <gwen.autencio@gmail.com>
2015-12-18 00:40:39 +1100

    test commit after change setings

--- learning-app/src/main/java/com/autencio/learning/configuration/ApplicationConfiguration.java
+++ learning-app/src/main/java/com/autencio/learning/configuration/ApplicationConfiguration.java
@@ -3,6 +3,8 @@ package com.autencio.learning.configuration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
+import com.mongodb.MongoClient;
+
 @Configuration
 @EnableAutoConfiguration
 public class ApplicationConfiguration {
