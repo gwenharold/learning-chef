6bce5af6c2edcc060242053f7c9b63c83d8099b7
Gwen Harold Autencio <gwen.autencio@gmail.com>
2015-12-22 23:59:10 +1100

    add recipe domain

---
+++ learning-app/src/main/java/com/autencio/learning/domain/Recipe.java
@@ -0,0 +1,37 @@
+package com.autencio.learning.domain;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="recipes")
+public class Recipe {
+
+	private String id;
+	
+	private String title;
+	
+	private String description;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
